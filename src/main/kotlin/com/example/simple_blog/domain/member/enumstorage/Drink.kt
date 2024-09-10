681b8e0955572de0d57aa0c47d95550d10cb71cc
Cha Cha <cha3088@gmail.com>
2024-09-10 12:43:44 +0900

    Feat: Added Member Properties (#10)
    * Feat: In the middle of making properties
    
    * Feat: Added Member Properties

--- src/main/kotlin/com/example/simple_blog/domain/member/enumstorage/Drink.kt
+++ src/main/kotlin/com/example/simple_blog/domain/member/enumstorage/Drink.kt
@@ -1,7 +1,7 @@
 package com.example.simple_blog.domain.member.enumstorage
 
-enum class HowFar {
-    CLOSE,
-    MIDDLE,
-    FAR,
+enum class Drink {
+    NEVER,
+    SOMETIMES,
+    OFTEN,
 }
--- src/main/kotlin/com/example/simple_blog/domain/member/enumstorage/Marry.kt
+++ src/main/kotlin/com/example/simple_blog/domain/member/enumstorage/Marry.kt
@@ -1,7 +1,6 @@
 package com.example.simple_blog.domain.member.enumstorage
 
-enum class HowFar {
-    CLOSE,
-    MIDDLE,
-    FAR,
+enum class Marry {
+    SINGLE,
+    DOLSING,
 }
