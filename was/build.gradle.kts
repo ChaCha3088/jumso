47bc92459ae58a4fe3e6a1edf0c1a855dd99813b
ChaCha3088 <cha3088@gmail.com>
2024-10-17 20:49:45 +0900

    Chore: WAS build.gradle 수정

--- was/build.gradle.kts
+++ was/build.gradle.kts
@@ -1,4 +1,15 @@
 plugins {
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.jpa") version "1.9.24"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.24"
+}
+
+repositories {
+    mavenCentral()
 }
 
 allOpen {
@@ -8,12 +19,42 @@ allOpen {
 }
 
 dependencies {
+    // JWT
+    implementation("com.auth0:java-jwt:4.4.0")
+    // Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // DataBase
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.mysql:mysql-connector-j:8.0.33")
+
+    // Tweeter Snowflake
+    implementation("com.littlenb:snowflake:1.0.5")
+
+    // Logging
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.1")
+
+    // ETC
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // Kotlin Faker
+    implementation("io.github.serpro69:kotlin-faker:1.16.0")
+
+    // AWS Parameter Store
+    implementation("io.awspring.cloud:spring-cloud-starter-aws-parameter-store-config:2.3.3")
+
+    // Test
+    testImplementation(kotlin("test"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
     // Security
         implementation("org.springframework.boot:spring-boot-starter-security")
 
-    // DataBase
-        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-        implementation("com.mysql:mysql-connector-j:8.0.33")
 
     // Kotlin JDSL
         implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter-jakarta:2.2.1.RELEASE")
@@ -22,4 +63,6 @@ dependencies {
 
     // Kafka
         implementation("org.springframework.kafka:spring-kafka")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
