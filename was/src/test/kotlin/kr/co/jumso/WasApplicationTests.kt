78d9845f7fe05de835c840316400d4516e0b957f
Cha Cha <cha3088@gmail.com>
2024-10-25 15:02:18 +0900

    Chore: 모듈 구조 개편 (#27)

--- was/src/test/kotlin/kr/co/jumso/WasApplicationTests.kt
+++ was/src/test/kotlin/kr/co/jumso/WasApplicationTests.kt
@@ -2,12 +2,12 @@ package kr.co.jumso
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("local")
 class WasApplicationTests {
-
 	@Test
 	fun contextLoads() {
 	}
-
 }
